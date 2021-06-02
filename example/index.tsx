ded85a07dc62d99bf191bbf297bf8a838e0f6f82
ZELLAT Abdelkhalek <ha_zellat@esi.dz>
2021-06-02 18:32:46 +0100

    Add Reactotron to example app

--- example/index.tsx
+++ example/index.tsx
@@ -3,3 +3,9 @@ import App from './src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure().useReactNative().connect();
+
+console.tron = Reactotron;

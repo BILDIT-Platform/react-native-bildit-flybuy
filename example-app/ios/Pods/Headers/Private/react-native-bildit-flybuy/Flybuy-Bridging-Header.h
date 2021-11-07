3c72c154c4f9e7d7dd11a4cd76ea85e1848152fc
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2021-11-07 15:57:58 +0100

    Rename example app

---
+++ example-app/ios/Pods/Headers/Private/react-native-bildit-flybuy/Flybuy-Bridging-Header.h
@@ -0,0 +1,1 @@
+../../../../../../ios/Flybuy-Bridging-Header.h
---
+++ example-app/ios/Pods/Target Support Files/Pods-FlybuyExample/Pods-FlybuyExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlybuyExample : NSObject
+@end
+@implementation PodsDummy_Pods_FlybuyExample
+@end
